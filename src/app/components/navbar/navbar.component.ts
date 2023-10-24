7af7690b1fa257b2b83917573e3219f984454bc8
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-10-24 19:35:18 -0400

    added navbar menu

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
+  sidenavOpen = false;
 
+  toggleSidenav() {
+    this.sidenavOpen = !this.sidenavOpen;
+  }
 }
