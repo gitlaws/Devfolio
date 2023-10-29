0c67aa2637c2c276c1cec223e7836c624a67d863
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-10-29 18:11:37 -0400

    fixed toggle menu btn

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,8 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss'],
 })
-export class NavbarComponent {}
+export class NavbarComponent {
+  menuBtn!: HTMLElement;
+  menuItems!: HTMLElement;
+  menuItem!: NodeListOf<Element>;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.menuBtn = document.querySelector('.menu-btn')!;
+    this.menuItems = document.querySelector('.menu-items')!;
+    this.menuItem = document.querySelectorAll('.menu-item');
+
+    if (this.menuBtn) {
+      this.menuBtn.addEventListener('click', () => {
+        this.toggle();
+      });
+    }
+
+    this.menuItem.forEach((item) => {
+      item.addEventListener('click', () => {
+        if (this.menuBtn && this.menuBtn.classList.contains('open')) {
+          this.toggle();
+        }
+      });
+    });
+  }
+
+  toggle() {
+    if (this.menuBtn) {
+      this.menuBtn.classList.toggle('open');
+    }
+    if (this.menuItems) {
+      this.menuItems.classList.toggle('open');
+    }
+  }
+}
