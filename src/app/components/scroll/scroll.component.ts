f571051751c905db109f3153031b5126c3f0859e
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-10-29 17:52:11 -0400

    todo

--- src/app/components/scroll/scroll.component.ts
+++ src/app/components/scroll/scroll.component.ts
@@ -5,4 +5,31 @@ import { Component } from '@angular/core';
   templateUrl: './scroll.component.html',
   styleUrls: ['./scroll.component.scss'],
 })
-export class ScrollComponent {}
+export class ScrollComponent {
+  constructor() {}
+
+  ngOnInit(): void {
+    const scrollers = document.querySelectorAll('.scroller');
+
+    if (!window.matchMedia('(prefers-reduced-motion: reduce)').matches) {
+      this.addAnimation(scrollers);
+    }
+  }
+
+  private addAnimation(scrollers: NodeListOf<Element>): void {
+    scrollers.forEach((scroller) => {
+      scroller.setAttribute('data-animated', 'true');
+
+      const scrollerInner = scroller.querySelector(
+        '.scroller__inner'
+      ) as Element;
+      const scrollerContent = Array.from(scrollerInner.children);
+
+      scrollerContent.forEach((item) => {
+        const duplicatedItem = (item as Element).cloneNode(true) as Element;
+        duplicatedItem.setAttribute('aria-hidden', 'true');
+        scrollerInner.appendChild(duplicatedItem);
+      });
+    });
+  }
+}
