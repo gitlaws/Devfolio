96f39ab26f15ef89fdf48cbd0bacb5d261a34fc1
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-12-12 20:11:02 -0500

    save

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -5,39 +5,47 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.scss'],
 })
-export class NavbarComponent {
-  menuBtn!: HTMLElement;
-  menuItems!: HTMLElement;
-  menuItem!: NodeListOf<Element>;
+export class NavbarComponent implements OnInit {
+  isDropdownOpen = false;
 
-  constructor() {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
-    this.menuBtn = document.querySelector('.menu-btn')!;
-    this.menuItems = document.querySelector('.menu-items')!;
-    this.menuItem = document.querySelectorAll('.menu-item');
-
-    if (this.menuBtn) {
-      this.menuBtn.addEventListener('click', () => {
-        this.toggle();
-      });
-    }
-
-    this.menuItem.forEach((item) => {
-      item.addEventListener('click', () => {
-        if (this.menuBtn && this.menuBtn.classList.contains('open')) {
-          this.toggle();
-        }
-      });
-    });
+  toggleDropdown() {
+    this.isDropdownOpen = !this.isDropdownOpen;
   }
 
-  toggle() {
-    if (this.menuBtn) {
-      this.menuBtn.classList.toggle('open');
-    }
-    if (this.menuItems) {
-      this.menuItems.classList.toggle('open');
-    }
-  }
+  // menuBtn!: HTMLElement;
+  // menuItems!: HTMLElement;
+  // menuItem!: NodeListOf<Element>;
+
+  // constructor() {}
+
+  // ngOnInit(): void {
+  //   this.menuBtn = document.querySelector('.menu-btn')!;
+  //   this.menuItems = document.querySelector('.menu-items')!;
+  //   this.menuItem = document.querySelectorAll('.menu-item');
+
+  //   if (this.menuBtn) {
+  //     this.menuBtn.addEventListener('click', () => {
+  //       this.toggle();
+  //     });
+  //   }
+
+  //   this.menuItem.forEach((item) => {
+  //     item.addEventListener('click', () => {
+  //       if (this.menuBtn && this.menuBtn.classList.contains('open')) {
+  //         this.toggle();
+  //       }
+  //     });
+  //   });
+  // }
+
+  // toggle() {
+  //   if (this.menuBtn) {
+  //     this.menuBtn.classList.toggle('open');
+  //   }
+  //   if (this.menuItems) {
+  //     this.menuItems.classList.toggle('open');
+  //   }
+  // }
 }
