c9040f67990c592f88011fc3e92681f8b0754c8b
Stanislaw M. Wasilewski <109121036+gitlaws@users.noreply.github.com>
2023-12-18 23:03:26 -0500

    save

--- src/app/services/theme.service.ts
+++ src/app/services/theme.service.ts
@@ -1,9 +1,38 @@
+// theme.service.ts
 import { Injectable } from '@angular/core';
 
+const THEME_KEY = 'app-theme';
+
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ThemeService {
+  private currentTheme!: string;
 
-  constructor() { }
+  constructor() {
+    this.loadTheme();
+  }
+
+  private loadTheme() {
+    this.currentTheme = localStorage.getItem(THEME_KEY) || 'light';
+    this.applyTheme();
+  }
+
+  getCurrentTheme() {
+    return this.currentTheme;
+  }
+
+  toggleTheme() {
+    this.currentTheme = this.currentTheme === 'light' ? 'dark' : 'light';
+    this.applyTheme();
+    localStorage.setItem(THEME_KEY, this.currentTheme);
+  }
+
+  private applyTheme() {
+    document.body.classList.toggle('dark-theme', this.currentTheme === 'dark');
+    document.body.classList.toggle(
+      'light-theme',
+      this.currentTheme === 'light'
+    );
+  }
 }
